d04af54c17aa5d06a07b2bc7304e9a0bd2d6e113
oxGrad <dev@graditya.com>
2026-04-23 15:29:25 +0000

    feat(nvim): non lazy knot

--- nvim/lua/plugins/knot.lua
+++ nvim/lua/plugins/knot.lua
@@ -1,7 +1,8 @@
 return {
   {
     "oxGrad/knot.nvim",
-    ft = "Knotfile",
+    main = "knot", -- tells lazy.nvim to call require("knot").setup(opts)
+    lazy = false, -- must load at startup so the devicon is registered before file trees render
     opts = {
       auto_configure_yamlls = true,
     },
